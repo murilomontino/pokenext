0e56f5761867ffcff4e0685a53363e63ed29de9f
Murilo Montino <51464383+murilomontino@users.noreply.github.com>
2023-05-02 23:49:16 +0000

    feat: card verde

--- source/components/molecules/card-pokemon/card-pokemon.tsx
+++ source/components/molecules/card-pokemon/card-pokemon.tsx
@@ -11,9 +11,10 @@ const CardPokemon = ({ url, name }: CardPokemonProps) => {
 
     const { pokemon, isLoading } = useFetchPokemon(url)
 
+
     return (
         <Link href={`/${name}`}>
-            <div key={name} className="flex-1 card">
+            <div key={name} className="flex-1 card bg-green-500">
                 {
                     pokemon && (
                         <Image src={pokemon?.sprites?.front_default} width={100} height={100} alt={pokemon?.name} />
--- source/pages/pokemon/pokemon.tsx
+++ source/pages/pokemon/pokemon.tsx
@@ -6,6 +6,8 @@ type PokemonProps = {
 }
 
 const Pokemon = ({ pokemon }: PokemonProps) => {
+    console.log(pokemon)
+
     return (
         <div>
             {
@@ -13,7 +15,21 @@ const Pokemon = ({ pokemon }: PokemonProps) => {
                     <Image src={pokemon?.sprites?.front_default} width={460} height={460} alt={pokemon?.name} />
                 )
             }
+            {
+                pokemon && (
+                    <Image src={pokemon?.sprites?.back_default} width={460} height={460} alt={pokemon?.name} />
+                )
+            }
             {pokemon?.name}
+            {pokemon?.types?.map(
+                (type, index) => (
+                <h1
+                    className="bg-black m-1"
+                    key={index}>
+                        {type.type.name}
+                        </h1>)
+            )
+            }
         </div>
     )
 }
