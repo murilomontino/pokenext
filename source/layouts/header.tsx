9dd7e05b798e96611f174343008eea739d21dd78
Murilo Montino <51464383+murilomontino@users.noreply.github.com>
2023-05-02 23:02:48 +0000

    feat: adicionar link ao button

--- source/layouts/header.tsx
+++ source/layouts/header.tsx
@@ -1,7 +1,16 @@
 import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
 import pokeball from '~/asserts/imgs/pokeball-hd.png'
-
+ 
 const Header = () => {
+    const [todo, setTodo] = useState(0)
+
+    useEffect(() => {}, [])
+
+    const a = 0
+
+
     return (
         <header
             className="
@@ -13,11 +22,14 @@ const Header = () => {
                 gap-2
             "
         >
-            <Image src={pokeball} width={50} height={50} alt='Logo Pokeball' className="ml-8" />
-
-            <h2 className="text-base font-bold text-center text-white font-mono">
-                PokeNext Estácio!
-            </h2>
+            
+            <Link href="/" className="flex justify-center items-center ">
+                <Image src={pokeball} width={50} height={50} alt='Logo Pokeball' className="ml-8" />
+            
+                <h2 className="text-base font-bold text-center text-white font-mono">
+                    Pokemons
+                </h2>
+            </Link>
         </header>
     )
 }
