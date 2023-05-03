38487b6ae54a942e853780188089c0790e8551e8
Keven Teles <glevenkeven@gmail.com>
2023-05-03 00:30:16 -0300

    feat: tipos e informações do pokemom

--- source/pages/pokemon/pokemon.tsx
+++ source/pages/pokemon/pokemon.tsx
@@ -1,21 +1,45 @@
-import Image from "next/image"
-import { Pokemon as IPokemon } from "~/types/pokemons"
+import Image from "next/image";
+import { Pokemon as IPokemon } from "~/types/pokemons";
 
 type PokemonProps = {
-    pokemon?: IPokemon
-}
+  pokemon?: IPokemon;
+};
+
 
 const Pokemon = ({ pokemon }: PokemonProps) => {
-    return (
+  
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        {pokemon && (
+          <Image
+            src={pokemon?.sprites?.front_default}
+            width={460}
+            height={460}
+            alt={pokemon?.name}
+          />
+        )}
+        {pokemon?.name}
+      </div>
+      <div className="flex flex-cols gap-2 flex-wrap flex-grow">
+      <div> 
+        {pokemon?.types.map((pokemons) =>(
+          <h1 key={pokemons.type.name}> Tipo do Pokemon: {pokemons.type.name}</h1>
+        ))
+        }
+        <h1> Base Experience : {pokemon?.base_experience}</h1>
         <div>
-            {
-                pokemon && (
-                    <Image src={pokemon?.sprites?.front_default} width={460} height={460} alt={pokemon?.name} />
-                )
-            }
-            {pokemon?.name}
+        {
+          pokemon?.abilities.map((hability) =>(
+            <h1 key={hability.ability.name}> Habilidades: {hability.ability.name}</h1>
+          ))
+        }
         </div>
-    )
-}
+      </div>
+      
+    </div>
+    </>
+  );
+};
 
-export default Pokemon
+export default Pokemon;
