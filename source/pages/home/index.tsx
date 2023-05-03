195689cd14426aba1ca0334ef695f79f7c9d252b
Keven Teles <glevenkeven@gmail.com>
2023-05-03 00:29:49 -0300

    feat: novos estados e função que filtra o pokemom

--- source/pages/home/index.tsx
+++ source/pages/home/index.tsx
@@ -6,12 +6,15 @@ import { ListPokemons } from "~/types/pokemons";
 
 const HomePage = () => {
   const [pokemons, setPokemons] = useState<ListPokemons[]>([]);
+  const [text, setText] = useState('')
+  const [query, setQuery] = useState<ListPokemons[]>([]);
 
   const fetchPokemons = async () => {
     const { status, data } = await api.get("/pokemon?limit=10000");
 
     if (status === 200) {
       setPokemons(data.results);
+      setQuery(data.results) 
     }
   };
 
@@ -19,12 +22,30 @@ const HomePage = () => {
     fetchPokemons();
   }, []);
 
+  useEffect(() => {
+    const filteredPokemons = ()=>{
+      return pokemons.filter((item) => (item.name.includes(text)))
+    }
+
+    if(!text){
+      setPokemons(query)
+    }else{
+      setPokemons(filteredPokemons)
+    }
+  }, [text]);
+
   return (
-    <div className="flex flex-cols gap-2 flex-wrap flex-grow">
+    <>
+    <div className="flex flex-col justify-center items-center">
+      <label htmlFor="">POKEAPI</label>
+      <input className="text-black rounded-lg p-2" value={text} onChange={(e)=>setText(e.target.value)} type="text" placeholder="Digite seu pokemon" />
+    </div>
+    <div className="flex gap-2 flex-wrap justify-center">
       {pokemons.slice(0, 153).map((pokemon) => (
         <CardPokemon key={pokemon.name} name={pokemon.name} url={pokemon.url} />
       ))}
     </div>
+    </>
   );
 };
 
