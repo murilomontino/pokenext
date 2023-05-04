eb7878a7283e6073224c32f4785bc0dc0517ac14
Keven Teles <glevenkeven@gmail.com>
2023-05-04 02:46:28 -0300

    feat: new componnet myButtons

--- source/components/molecules/geration-pokemon/geration-pokemon.tsx
+++ source/components/molecules/geration-pokemon/geration-pokemon.tsx
@@ -1,27 +1,15 @@
-import { Tab } from '@headlessui/react';
-import React, { useState } from 'react';
 
-type TabPokemons = {
-  children: React.ReactNode
+type buttonPokemons = {
+  setActiveIndex: (index: number) => void
 }
 
-export default function MyTabs ({ children }: TabPokemons) {
-  const [currentTab, setCurrentTab] = useState(0);
+export default function MyButtons({ setActiveIndex }: buttonPokemons) {
 
   return (
-    <Tab.Group>
-      <Tab.List className='flex space-x-1 '>
-        <Tab className={`bg-blue-900/20 p-1 rounded-lg ${currentTab === 0 ? 'bg-blue-900' : ''}`} onClick={() => setCurrentTab(0)}>Primeira Geração</Tab>
-        <Tab className={`bg-blue-900/20 p-1 rounded-lg ${currentTab === 1 ? 'bg-blue-900' : ''}`} onClick={() => setCurrentTab(1)}>Segunda Geração</Tab>
-        <Tab className={`bg-blue-900/20 p-1 rounded-lg ${currentTab === 2 ? 'bg-blue-900' : ''}`} onClick={() => setCurrentTab(2)}>Terceira Geração</Tab>
-      </Tab.List>
-      <Tab.Panels className='mt-2'>
-        {React.Children.toArray(children).map((child, index) => (
-          <Tab.Panel className={currentTab === index ? 'block' : 'hidden'}>
-            {child}
-          </Tab.Panel>
-        ))}
-      </Tab.Panels>
-    </Tab.Group>
+    <div>
+      <button className={"bg-blue-900/20 p-1 rounded-lg"} onClick={() => setActiveIndex(1)}>Primeira Geração</button>
+      <button className={"bg-blue-900/20 p-1 rounded-lg"} onClick={() => setActiveIndex(2)}>Segunda Geração</button>
+      <button className={"bg-blue-900/20 p-1 rounded-lg"} onClick={() => setActiveIndex(3)}>Terceira Geração</button>
+    </div>
   )
 }
