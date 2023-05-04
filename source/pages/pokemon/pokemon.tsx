fb9951678377d50be68f6df6bcb62ee9bf0ba4f3
Keven Teles <glevenkeven@gmail.com>
2023-05-04 02:47:52 -0300

    feat: defferead value

--- source/pages/pokemon/pokemon.tsx
+++ source/pages/pokemon/pokemon.tsx
@@ -7,13 +7,13 @@ type PokemonProps = {
 
 
 const Pokemon = ({ pokemon }: PokemonProps) => {
-  
+
   return (
     <>
       <div className="flex flex-col justify-center items-center">
         {pokemon && (
           <Image
-            src={pokemon?.sprites?.front_default}
+            src={pokemon?.sprites.front_default}
             width={460}
             height={460}
             alt={pokemon?.name}
@@ -22,22 +22,22 @@ const Pokemon = ({ pokemon }: PokemonProps) => {
         {pokemon?.name}
       </div>
       <div className="flex flex-cols gap-2 flex-wrap flex-grow">
-      <div> 
-        {pokemon?.types.map((pokemons) =>(
-          <h1 key={pokemons.type.name}> Tipo do Pokemon: {pokemons.type.name}</h1>
-        ))
-        }
-        <h1> Base Experience : {pokemon?.base_experience}</h1>
         <div>
-        {
-          pokemon?.abilities.map((hability) =>(
-            <h1 key={hability.ability.name}> Habilidades: {hability.ability.name}</h1>
+          {pokemon?.types.map((pokemons) => (
+            <h1 key={pokemons.type.name}> Tipo do Pokemon: {pokemons.type.name}</h1>
           ))
-        }
+          }
+          <h1> Base Experience : {pokemon?.base_experience}</h1>
+          <div>
+            {
+              pokemon?.abilities.map((hability) => (
+                <h1 key={hability.ability.name}> Habilidades: {hability.ability.name}</h1>
+              ))
+            }
+          </div>
         </div>
+
       </div>
-      
-    </div>
     </>
   );
 };
