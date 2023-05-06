d192d8944708938feaad858ff8d90479e061c02a
Keven Teles <glevenkeven@gmail.com>
2023-05-06 18:33:23 -0300

    feat: new button style

--- source/components/molecules/geration-pokemon/geration-pokemon.tsx
+++ source/components/molecules/geration-pokemon/geration-pokemon.tsx
@@ -10,10 +10,10 @@ export default function MyButtons({ setActiveIndex }: buttonPokemons) {
     //   <button className={"bg-blue-900/20 p-2 rounded-lg"} onClick={() => setActiveIndex(1)}>Primeira Geração</button>
     //   <button className={"bg-blue-900/20 p-2 rounded-lg"} onClick={() => setActiveIndex(2)}>Segunda Geração</button>
     //   <button className={"bg-blue-900/20 p-2 rounded-lg"} onClick={() => setActiveIndex(3)}>Terceira Geração</button>
-    <div className="btn-group">
-      <button className="`${btn btn-active}`" onClick={() => setActiveIndex(1)} >Button</button>
-      <button className="btn" onClick={() => setActiveIndex(2)}>Button</button>
-      <button className="btn" onClick={() => setActiveIndex(3)} >Button</button>
+    <div className="btn-group gap-1 flex justify-center">
+      <button className="btn" onClick={() => setActiveIndex(1)} >1° Gen</button>
+      <button className="btn" onClick={() => setActiveIndex(2)}>2° Gen</button>
+      <button className="btn" onClick={() => setActiveIndex(3)} >3° Gen</button>
     </div>
     // </div>
   )
