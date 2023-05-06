4ac8c8689a3fec86ae509136148d59e992a2b64e
Keven Teles <glevenkeven@gmail.com>
2023-05-06 17:39:03 -0300

    feat: types pokemon in card

--- source/components/molecules/card-pokemon/card-pokemon.tsx
+++ source/components/molecules/card-pokemon/card-pokemon.tsx
@@ -13,13 +13,23 @@ const CardPokemon = ({ url, name }: CardPokemonProps) => {
 
     return (
         <Link href={`/${name}`}>
-            <div key={name} className={`flex-1 card ${pokemon?.types[0].type.name}`}>
+            <div key={name} className={`flex-1  card ${pokemon?.types[0].type.name}`}>
+                <div className="flex gap-2">
+                    {pokemon?.types?.map((pokemons) => (
+
+                        <div className={`text-white ${pokemon?.types[0].type.name} my-1 mx-0 brightness-110 rounded-lg text-[10px] py-1 px-2`}>
+                            <span key={pokemons?.type?.name}>{pokemons?.type?.name}</span>
+                        </div>
+                    ))
+                    }
+                </div>
+
                 {
                     pokemon?.sprites?.other.home.front_default && (
                         <Image src={pokemon?.sprites?.other.home.front_default} width={100} height={100} alt={pokemon?.name} />
                     )
                 }
-                <h2>{pokemon?.name}</h2>
+                <h2 className="text-white">{pokemon?.name}</h2>
             </div>
         </Link>
     )
