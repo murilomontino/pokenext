2be6be56e6808cca7dedafc1977d59c3a8c1ccf4
Keven Teles <glevenkeven@gmail.com>
2023-05-06 18:31:45 -0300

    feat: new footer

--- source/layouts/footer.tsx
+++ source/layouts/footer.tsx
@@ -1,7 +1,11 @@
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className="footer footer-center p-4 bg-base-300  text-white">
+            <div>
+                <p>Copyright © 2023 - Forked for murilomonitno, coded by KevenDev </p>
+            </div>
+        </footer>
     )
 }
 
