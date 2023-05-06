0670e004c2445309a6692a0a9d4d16d436aad4d8
Keven Teles <glevenkeven@gmail.com>
2023-05-06 18:32:32 -0300

    feat: new pokemon style

--- source/pages/pokemon/pokemon.tsx
+++ source/pages/pokemon/pokemon.tsx
@@ -12,22 +12,27 @@ const Pokemon = ({ pokemon }: PokemonProps) => {
     <>
       <div className={`rounded-xl flex flex-col justify-center items-center ${pokemon?.types[0].type.name}`}>
         <div className="flex flex-col justify-center items-center">
+          <h1 className="justify-start mt-4 text-xl">{pokemon?.name}</h1>
+          {pokemon?.types?.map((pokemons) => (
+
+            <div className={`text-white ${pokemon?.types[0].type.name} my-1 mx-0 brightness-110 rounded-lg text-[10px] py-1 px-2`}>
+              <span key={pokemons?.type?.name}>{pokemons?.type?.name}</span>
+            </div>
+          ))
+          }
           {pokemon && (
             <Image
-              src={pokemon?.sprites?.front_default ?? ""}
-              width={460}
-              height={460}
+              className="p-10"
+              src={pokemon?.sprites?.other.dream_world.front_default ?? ""}
+              width={240}
+              height={450}
               alt={pokemon?.name}
             />
           )}
-          {pokemon?.name}
+
         </div>
-        <div className="flex flex-cols gap-2 flex-wrap flex-grow p-4">
+        <div className="flex flex-cols gap-2 flex-wrap flex-grow p-4 bg-white w-full text-black">
           <div>
-            {pokemon?.types?.map((pokemons) => (
-              <h1 key={pokemons?.type?.name}> Tipo do Pokemon: {pokemons?.type?.name}</h1>
-            ))
-            }
             <h1> Base Experience : {pokemon?.base_experience}</h1>
             <div>
               {
