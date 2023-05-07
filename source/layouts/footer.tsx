83a2ccca579e9b0934c62aa0b6c345a936888e81
Keven Teles <glevenkeven@gmail.com>
2023-05-07 13:30:51 -0300

    feat: change footer desc

--- source/layouts/footer.tsx
+++ source/layouts/footer.tsx
@@ -1,9 +1,9 @@
 
 const Footer = () => {
     return (
-        <footer className="footer footer-center p-4 bg-base-300  text-white">
+        <footer className="footer footer-center p-4 bg-base-300 text-white">
             <div>
-                <p>Copyright © 2023 - Forked for murilomonitno, coded by KevenDev </p>
+                <p>Copyright © 2023 - Forked for <a className="text-blue-500" href="https://github.com/murilomontino/pokenext" target="_blank">murilomontino</a> , coded by <a className="text-blue-500" href="https://github.com/KevenDev/pokenext-estacio" target="_blank"> KevenDev</a>  </p>
             </div>
         </footer>
     )
