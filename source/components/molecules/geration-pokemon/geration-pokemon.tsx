df8f6a4e7831d7cafc435ec478224f5dd799f93a
Keven Teles <glevenkeven@gmail.com>
2023-05-07 16:20:50 -0300

    feat: effect button

--- source/components/molecules/geration-pokemon/geration-pokemon.tsx
+++ source/components/molecules/geration-pokemon/geration-pokemon.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 
 type buttonPokemons = {
   setActiveIndex: (index: number) => void
@@ -5,11 +6,19 @@ type buttonPokemons = {
 
 export default function MyButtons({ setActiveIndex }: buttonPokemons) {
 
+  const [colorButton, setColorButton] = useState(1)
+
+  const handleButton = (index: number) => {
+    setColorButton(index)
+    setActiveIndex(index)
+  }
+
+
   return (
     <div className="btn-group gap-1 flex justify-center">
-      <button className="btn" onClick={() => setActiveIndex(1)} >1° Gen</button>
-      <button className="btn" onClick={() => setActiveIndex(2)}>2° Gen</button>
-      <button className="btn" onClick={() => setActiveIndex(3)} >3° Gen</button>
+      <button className={`btn ${colorButton === 1 ? "btn-active" : ""}`} onClick={() => handleButton(1)} >1° Gen</button>
+      <button className={`btn ${colorButton === 2 ? "btn-active" : ""}`} onClick={() => handleButton(2)}>2° Gen</button>
+      <button className={`btn ${colorButton === 3 ? "btn-active" : ""}`} onClick={() => handleButton(3)} >3° Gen</button>
     </div>
   )
 }
